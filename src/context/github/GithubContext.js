d0b3cede3f9d54fb1067bf744a5de7e7907f3cec
Seth Kingsbury <sethkingsbury@gmail.com>
2022-07-27 15:35:34 +1200

    Finished github finder project. main react concepts covered were context, reducers, action files. Also introduced axios to clean up HTTP requests.

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,73 +3,21 @@ import GithubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 export const GithubProvider = ({ children }) => {
 	const initialState = {
 		users: [],
 		user: {},
+		repos: [],
 		loading: false,
 	};
 
 	const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-	const searchUsers = async (text) => {
-		setLoading();
-
-		const params = new URLSearchParams({
-			q: text,
-		});
-
-		const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-			headers: {
-				Authorization: `token ${GITHUB_TOKEN}`,
-			},
-		});
-
-		const { items } = await res.json();
-
-		dispatch({
-			type: 'GET_USERS',
-			payload: items,
-		});
-	};
-
-	const getUser = async (login) => {
-		setLoading();
-
-		const res = await fetch(`${GITHUB_URL}/users/${login}`, {
-			headers: {
-				Authorization: `token ${GITHUB_TOKEN}`,
-			},
-		});
-
-		if (res.status === 404) {
-			window.location = '/notfound';
-		} else {
-			const data = await res.json();
-
-			dispatch({
-				type: 'GET_USER',
-				payload: data,
-			});
-		}
-	};
-
-	const setLoading = () => dispatch({ type: 'SET_LOADING' });
-
-	const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
-
 	return (
 		<GithubContext.Provider
 			value={{
-				users: state.users,
-				loading: state.loading,
-				user: state.user,
-				searchUsers,
-				clearUsers,
-				getUser,
+				...state,
+				dispatch,
 			}}
 		>
 			{children}
